9e035e9cf5e8ec9b4f441f7eff086c58a1f6f86d
Lc <24790549@qq.com>
2019-06-10 16:02:35 +0800

    Demo1

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -4,12 +4,12 @@ import org.testng.annotations.Test;
 public class demo {
 
     @Test
-    public void TestSuccess(){
+    public void Success(){
         System.out.println("演示1");
     }
 
     @Test
-    public void TestFail(){
+    public void Fail(){
 
         Assert.assertEquals(1,2);
     }
